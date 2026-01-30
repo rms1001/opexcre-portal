de9a4b6d245660cf92d960e52c6681b56f7ad6cf
rms1001 <rmstein100@aol.com>
2026-01-30 08:29:52 -0600

    Remove PropertyProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { PropertyProvider } from "./context/PropertyContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PropertyProvider>
-      <App />
-    </PropertyProvider>
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+      );
